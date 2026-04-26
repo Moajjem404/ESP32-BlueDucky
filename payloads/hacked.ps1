4441d6bfeaa9ab5cb5ebd8339c5fd0ed7d880ad9
Md Moajjem Hossen <82633525+Moajjem404@users.noreply.github.com>
2026-04-27 02:18:21 +0600

    Update hacked.ps1

--- payloads/hacked.ps1
+++ payloads/hacked.ps1
@@ -1,16 +1,24 @@
 Clear-Host
 Write-Host @"
-__   __            _   _                 ___                  _  _         _         _ 
-\ \ / /__ _  _    | |_| |__ ___ _____   | _ ) ___ ___ _ _    | || |__ _ __| |_____ _| |
- \ V / _ \ || |   |  _  / _` \ V / -_)  | _ \/ -_) -_) ' \   | __ / _` / _| / / -_) _` |
-  |_|\___/\_,_|   |_| |_\__,_|\_/\___|  |___/\___\___|_||_|  |_||_\__,_\__|_\_\___\__,_|
+ __   __  ___   _   _    _   _    _    __     __  _____   ____   _____  _____  _   _ 
+ \ \ / / / _ \ | | | |  | | | |  / \   \ \   / / | ____| | __ ) | ____|| ____|| \ | |
+  \ V / | | | || | | |  | |_| | / _ \   \ \ / /  |  _|   |  _ \ |  _|  |  _|  |  \| |
+   | |  | |_| || |_| |  |  _  |/ ___ \   \ V /   | |___  | |_) || |___ | |___ | |\  |
+   |_|   \___/  \___/   |_| |_/_/   \_\   \_/    |_____| |____/ |_____||_____||_| \_|
+                                                                                     
+  _   _      _      ____  _  __  _____  ____  
+ | | | |    / \    / ___|| |/ / | ____||  _ \ 
+ | |_| |   / _ \  | |    | ' /  |  _|  | | | |
+ |  _  |  / ___ \ | |___ | . \  | |___ | |_| |
+ |_| |_| /_/   \_\ \____||_|\_\ |_____||____/ 
 "@ -ForegroundColor Green
 
 Write-Host ""
 Write-Host "==========================================================" -ForegroundColor Red
-Write-Host "           SYSTEM COMPROMISED BY ESP32 BLUEDUCKY   PaCK       " -ForegroundColor White -BackgroundColor DarkRed
+Write-Host "           SYSTEM COMPROMISED BY ESP32 BLUEDUCKY          " -ForegroundColor White -BackgroundColor DarkRed
 Write-Host "==========================================================" -ForegroundColor Red
 Write-Host ""
 Write-Host "Don't worry, this is just a friendly prank! :)" -ForegroundColor Yellow
 Write-Host ""
-
+Start-Sleep -Seconds 10
+Exit
