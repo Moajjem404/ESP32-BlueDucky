74e279be6ebe2fa6eada661ae5cb24ae3d5a6b9a
Md Moajjem Hossen <82633525+Moajjem404@users.noreply.github.com>
2026-04-28 00:41:42 +0600

    Update hacked.ps1

--- payloads/hacked.ps1
+++ payloads/hacked.ps1
@@ -1,19 +1,25 @@
-[console]::OutputEncoding = [System.Text.Encoding]::UTF8
 Clear-Host
-Write-Host @"
-__   ______  _   _   _   _  ___  _   _ _____   ____  _____ _____ _   _   _   _  ___  ____ _  __ _____ ____  
-\ \ / / __ \| | | | | | | |/ _ \| | | | ____| | __ )| ____| ____| \ | | | | | |/ _ \/ ___| |/ /| ____|  _ \ 
- \ V / |  | | | | | | |_| | |_| | | | |  _|   |  _ \|  _| |  _| |  \| | | |_| | |_| | |  | ' / |  _| | | | |
-  | || |__| | |_| | |  _  |  _  | |_| | |___  | |_) | |___| |___| |\  | |  _  |  _  | |__| . \ | |___| |_| |
-  |_| \____/ \___/  |_| |_|_| |_|\___/|_____| |____/|_____|_____|_| \_| |_| |_|_| |_|\____|_|\_\|_____|____/ 
-"@ -ForegroundColor Green
-
 Write-Host ""
-Write-Host "==========================================================" -ForegroundColor Red
-Write-Host "           SYSTEM COMPROMISED BY ESP32 BLUEDUCKY          " -ForegroundColor White -BackgroundColor DarkRed
-Write-Host "==========================================================" -ForegroundColor Red
+Write-Host " __   _____  _   _   _   _    _    _   _ _____ " -ForegroundColor Red
+Write-Host " \ \ / / _ \| | | | | | | |  / \  | | | | ____|" -ForegroundColor Red
+Write-Host "  \ V / (_) | |_| | | |_| | / _ \ | | | |  _|  " -ForegroundColor Red
+Write-Host "   | | \___/ \___/  |  _  |/ ___ \| |_| | |___ " -ForegroundColor DarkRed
+Write-Host "   |_|              |_| |_/_/   \_\\___/|_____|" -ForegroundColor DarkRed
 Write-Host ""
-Write-Host "Don't worry, this is just a friendly prank! :)" -ForegroundColor Yellow
+Write-Host "  ____  _____ _____ _   _   _   _    _    ____ _  _______ ____  " -ForegroundColor Red
+Write-Host " | __ )| ____| ____| \ | | | | | |  / \  / ___| |/ / ____|  _ \ " -ForegroundColor Red
+Write-Host " |  _ \|  _| |  _| |  \| | | |_| | / _ \| |   | ' /|  _| | | | |" -ForegroundColor Red
+Write-Host " | |_) | |___| |___| |\  | |  _  |/ ___ \ |___| . \| |___| |_| |" -ForegroundColor DarkRed
+Write-Host " |____/|_____|_____|_| \_| |_| |_/_/   \_\____|_|\_\_____|____/ " -ForegroundColor DarkRed
+Write-Host ""
+Write-Host "================================================================" -ForegroundColor DarkGray
+Write-Host "             SYSTEM COMPROMISED BY ESP32 BLUEDUCKY              " -ForegroundColor White -BackgroundColor DarkRed
+Write-Host "================================================================" -ForegroundColor DarkGray
+Write-Host ""
+Write-Host " [!] Unauthorized access detected." -ForegroundColor Yellow
+Write-Host " [!] Data exfiltration initiated..." -ForegroundColor Yellow
+Write-Host ""
+Write-Host " Don't worry, this is just a friendly prank! :)" -ForegroundColor Green
 Write-Host ""
 Start-Sleep -Seconds 10
 Exit
