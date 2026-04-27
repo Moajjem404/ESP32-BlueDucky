92010ad4a10a1bd9d651592262964729c1a0ef32
Md Moajjem Hossen <82633525+Moajjem404@users.noreply.github.com>
2026-04-28 00:43:56 +0600

    Update hacked.ps1

--- payloads/hacked.ps1
+++ payloads/hacked.ps1
@@ -1,25 +1,27 @@
 Clear-Host
+$b = [char]27 + "[1m"
+$r = [char]27 + "[0m"
 Write-Host ""
-Write-Host " __   _____  _   _   _   _    _    _   _ _____ " -ForegroundColor Red
-Write-Host " \ \ / / _ \| | | | | | | |  / \  | | | | ____|" -ForegroundColor Red
-Write-Host "  \ V / (_) | |_| | | |_| | / _ \ | | | |  _|  " -ForegroundColor Red
-Write-Host "   | | \___/ \___/  |  _  |/ ___ \| |_| | |___ " -ForegroundColor DarkRed
-Write-Host "   |_|              |_| |_/_/   \_\\___/|_____|" -ForegroundColor DarkRed
+Write-Host "$b __   _____  _   _   _   _    _    _   _ _____ $r" -ForegroundColor Red
+Write-Host "$b \ \ / / _ \| | | | | | | |  / \  | | | | ____|$r" -ForegroundColor Red
+Write-Host "$b  \ V / (_) | |_| | | |_| | / _ \ | | | |  _|  $r" -ForegroundColor Red
+Write-Host "$b   | | \___/ \___/  |  _  |/ ___ \| |_| | |___ $r" -ForegroundColor DarkRed
+Write-Host "$b   |_|              |_| |_/_/   \_\\___/|_____|$r" -ForegroundColor DarkRed
 Write-Host ""
-Write-Host "  ____  _____ _____ _   _   _   _    _    ____ _  _______ ____  " -ForegroundColor Red
-Write-Host " | __ )| ____| ____| \ | | | | | |  / \  / ___| |/ / ____|  _ \ " -ForegroundColor Red
-Write-Host " |  _ \|  _| |  _| |  \| | | |_| | / _ \| |   | ' /|  _| | | | |" -ForegroundColor Red
-Write-Host " | |_) | |___| |___| |\  | |  _  |/ ___ \ |___| . \| |___| |_| |" -ForegroundColor DarkRed
-Write-Host " |____/|_____|_____|_| \_| |_| |_/_/   \_\____|_|\_\_____|____/ " -ForegroundColor DarkRed
+Write-Host "$b  ____  _____ _____ _   _   _   _    _    ____ _  _______ ____  $r" -ForegroundColor Red
+Write-Host "$b | __ )| ____| ____| \ | | | | | |  / \  / ___| |/ / ____|  _ \ $r" -ForegroundColor Red
+Write-Host "$b |  _ \|  _| |  _| |  \| | | |_| | / _ \| |   | ' /|  _| | | | |$r" -ForegroundColor Red
+Write-Host "$b | |_) | |___| |___| |\  | |  _  |/ ___ \ |___| . \| |___| |_| |$r" -ForegroundColor DarkRed
+Write-Host "$b |____/|_____|_____|_| \_| |_| |_/_/   \_\____|_|\_\_____|____/ $r" -ForegroundColor DarkRed
 Write-Host ""
-Write-Host "================================================================" -ForegroundColor DarkGray
-Write-Host "             SYSTEM COMPROMISED BY ESP32 BLUEDUCKY              " -ForegroundColor White -BackgroundColor DarkRed
-Write-Host "================================================================" -ForegroundColor DarkGray
+Write-Host "$b================================================================$r" -ForegroundColor DarkGray
+Write-Host "$b             SYSTEM COMPROMISED BY ESP32 BLUEDUCKY              $r" -ForegroundColor White -BackgroundColor DarkRed
+Write-Host "$b================================================================$r" -ForegroundColor DarkGray
 Write-Host ""
-Write-Host " [!] Unauthorized access detected." -ForegroundColor Yellow
-Write-Host " [!] Data exfiltration initiated..." -ForegroundColor Yellow
+Write-Host "$b [!] Unauthorized access detected.$r" -ForegroundColor Yellow
+Write-Host "$b [!] Data exfiltration initiated...$r" -ForegroundColor Yellow
 Write-Host ""
-Write-Host " Don't worry, this is just a friendly prank! :)" -ForegroundColor Green
+Write-Host "$b Don't worry, this is just a friendly prank! :)$r" -ForegroundColor Green
 Write-Host ""
 Start-Sleep -Seconds 10
 Exit
